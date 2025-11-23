c9503b7f4d2d30b97c0b4fbda56b0e7da2c58f4f
dakshkasi <125728599+GeniusDuckk@users.noreply.github.com>
2025-11-22 19:20:56 -0600

    Making the gemini prompt better

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
-GEMINI_API_KEY = 'AIzaSyDdAQGzJ0nHkfixQn5kGETA-oKKwn9skew'
-
 from google import genai
 from PIL import Image
 
-sample_image_path = './media/frames/frame_0001.png'
+def gemini_text():
+    GEMINI_API_KEY = 'AIzaSyDdAQGzJ0nHkfixQn5kGETA-oKKwn9skew'
 
-# Load the image using the Pillow (PIL) library
-sample_image = Image.open(sample_image_path)
+    sample_image_path = './media/frames/frame_0001.png'
 
-# The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key=GEMINI_API_KEY)
+    # Load the image using the Pillow (PIL) library
+    sample_image = Image.open(sample_image_path)
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", 
-    # contents="What is the difference between a partial order relation and other relations"
-    contents=[
-        sample_image,
-        "What is everything you see in this image"
-    ]
-)
-print(response.text)
+    # The client gets the API key from the environment variable `GEMINI_API_KEY`.
+    client = genai.Client(api_key=GEMINI_API_KEY)
+
+    prompt = """You are an attentive, practical, and highly detailed personal guide. Your task is to
+            provide a real-time, functional audio description of the immediate environment captured
+            in the image. The description must be clear, concise, actionable, and focused on spatial
+            orientation, obstacles, and the immediate proximity of people or objects.
+            Begin with the most critical safety information (e.g., ground level, major obstacles). 
+            Then, describe the immediate foreground (within three steps), focusing on their location 
+            relative to the viewer (e.g., 'to your left') and their potential use or hazard. 
+            Conclude with the general setting and any notable ambient conditions (e.g., busy,
+            quiet, light source)."""
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", 
+        # contents="What is the difference between a partial order relation and other relations"
+        contents=[
+            sample_image,
+            prompt
+        ]
+    )
+    
+    return response.text
