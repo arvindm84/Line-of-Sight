c2fe7c57e6e867a9b5d3655dc5ed6cac66ac925e
Sreevatsa Surya Tanikella <tanikella.sreevatsa@gmail.com>
2025-11-22 19:31:36 -0600

    Add Call to Gemini_Text() for Response

---
+++ fish.py
@@ -0,0 +1,17 @@
+from fishaudio import FishAudio
+from fishaudio.utils import save
+import generate_audio_script
+
+# Initialize with your API key
+client = FishAudio(api_key="b34f820bdc61448e96e1235f94fa60d0") # Sreevatsa's API KEY
+
+response = generate_audio_script.gemini_text()
+
+print(response)
+
+# Generate speech
+audio = client.tts.convert(text=response)
+save(audio, "welcome.mp3")
+
+print("✓ Audio saved to welcome.mp3")
+
